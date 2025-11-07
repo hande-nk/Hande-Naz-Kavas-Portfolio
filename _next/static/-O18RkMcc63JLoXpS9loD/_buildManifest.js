6e94212ba46d65e53a22952ce382cbe68cafa3f6
hande-nk <handenazkavas@gmail.com>
2025-11-07 11:10:08 -0500

    Static export fix for GitHub Pages

---
+++ _next/static/-O18RkMcc63JLoXpS9loD/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Hande-Naz-Kavas-Portfolio//_next/:path+",
+        "destination": "/Hande-Naz-Kavas-Portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
