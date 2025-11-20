d90834c27701d6dbc1d96456f132a16a2e7e437c
hande-nk <handenazkavas@gmail.com>
2025-11-19 23:10:30 -0500

    Correct Tailwind + PostCSS config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,11 @@ const config: Config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
 export default config;
