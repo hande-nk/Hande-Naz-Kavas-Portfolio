949d848b5dfc603bf4071c65b1b1ebca087a07bb
hande-nk <handenazkavas@gmail.com>
2025-11-19 23:15:26 -0500

    new changes

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
-
 const nextConfig: NextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  trailingSlash: true,
-  
-
-  //  GitHub Pages base path fix
-  basePath: isProd ? "/Hande-Naz-Kavas-Portfolio" : "",
-  assetPrefix: isProd ? "/Hande-Naz-Kavas-Portfolio/" : "",
-
-
-  //reactCompiler: true,
-
-  
+  /* config options here */
+  reactCompiler: true,
 };
 
 export default nextConfig;
